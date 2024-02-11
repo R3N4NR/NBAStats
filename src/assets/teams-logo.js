2a475aa1b33e89beffccb51a28863ec589a2e675
R3N4NR <renanramalhou@gmail.com>
2024-02-10 21:23:00 -0300

    altera os caminhos das imagens

--- src/assets/teams-logo.js
+++ src/assets/teams-logo.js
@@ -1,122 +1,122 @@
 export const teams = [
     {
-       picture_path : '../.tmp', 
+      picture_path: '../../src/assets/images/nba-logos/hawks.png', 
       name: 'Hawks',
       away: 'Lakers'
     },
     {
-       picture_path : '../.tmp', 
+       picture_path : '../../../assets/images/nba-logos/nets.png', 
       name: 'Nets'
     },
     {
-       picture_path : '../.tmp', 
+       picture_path : '../../../assets/images/nba-logos/celtics.png', 
       name: 'Celtics'
     },
     {
-       picture_path : '../.tmp', 
+       picture_path : '../../../assets/images/nba-logos/hornets.png', 
       name: 'Hornets'
     },
     {
-       picture_path : '../.tmp', 
+       picture_path : '../../../assets/images/nba-logos/bulls.png', 
       name: 'Bulls'
     },
     {
-       picture_path : '../.tmp', 
+       picture_path : '../../../assets/images/nba-logos/cavaliers.png', 
       name: 'Cavaliers'
     },
     {
-       picture_path : '../.tmp', 
+       picture_path : '../../../assets/images/nba-logos/mavericks.png', 
       name: 'Mavericks'
     },
     {
-       picture_path : '../.tmp', 
+       picture_path : '../../../assets/images/nba-logos/nuggets.png', 
       name: 'Nuggets'
     },
     {
-       picture_path : '../.tmp', 
+       picture_path : '../../../assets/images/nba-logos/pistons.png', 
       name: 'Pistons'
     },
     {
-       picture_path : '../.tmp', 
+       picture_path : '../../../assets/images/nba-logos/warriors.png', 
       name: 'Warriors'
     },
     {
-       picture_path : '../.tmp', 
+       picture_path : '../../../assets/images/nba-logos/rockets.png', 
       name: 'Rockets'
     },
     {
-       picture_path : '../.tmp', 
+       picture_path : '../../../assets/images/nba-logos/pacers.png', 
       name: 'Pacers'
     },
     {
-       picture_path : '../.tmp', 
+       picture_path : '../../../assets/images/nba-logos/clippers.png', 
       name: 'Clippers'
     },
     {
-       picture_path : '../.tmp', 
+       picture_path : '../../../assets/images/nba-logos/laker.png', 
       name: 'Lakers'
     },
     {
-       picture_path : '../.tmp', 
+       picture_path : '../../../assets/images/nba-logos/grizzlies.png', 
       name: 'Grizzlies'
     },
     {
-       picture_path : '../.tmp', 
+       picture_path : '../../../assets/images/nba-logos/heat.png', 
       name: 'Heat'
     },
     {
-       picture_path : '../.tmp', 
+       picture_path : '../../../assets/images/nba-logos/Bucks.png', 
       name: 'Bucks'
     },
     {
-       picture_path : '../.tmp', 
+       picture_path : '../../../assets/images/nba-logos/timberwolves.png', 
       name: 'Timberwolves'
     },
     {
-       picture_path : '../.tmp', 
+       picture_path : '../../../assets/images/nba-logos/pelicans.png', 
       name: 'Pelicans'
     },
     {
-       picture_path : '../.tmp', 
+       picture_path : '../../../assets/images/nba-logos/knicks.png', 
       name: 'Knicks'
     },
     {
-       picture_path : '../.tmp', 
+       picture_path : '../../../assets/images/nba-logos/okc.png', 
       name: 'Thunder'
     },
     {
-       picture_path : '../.tmp', 
+       picture_path : '../../../assets/images/nba-logos/magic.png', 
       name: 'Magic'
     },
     {
-       picture_path : '../.tmp', 
+       picture_path : '../../../assets/images/nba-logos/76ers.png', 
       name: '76ers'
     },
     {
-       picture_path : '../.tmp', 
+       picture_path : '../../../assets/images/nba-logos/suns.png', 
       name: 'Suns'
     },
     {
-       picture_path : '../.tmp', 
+       picture_path : '../../../assets/images/nba-logos/blazers.png', 
       name: 'Blazers'
     },
     {
-       picture_path : '../.tmp', 
+       picture_path : '../../../assets/images/nba-logos/kings.png', 
       name: 'Kings'
     },
     {
-       picture_path : '../.tmp', 
+       picture_path : '../../../assets/images/nba-logos/spurs.png', 
       name: 'Spurs'
     },
     {
-       picture_path : '../.tmp', 
+       picture_path : '../../../assets/images/nba-logos/raptors.png', 
       name: 'Raptors'
     },
     {
-       picture_path : '../.tmp', 
+       picture_path : '../../../assets/images/nba-logos/jazz.png', 
       name: 'Jazz'
     },
-    { picture_path : '../.tmp', 
+    { picture_path : '../../../assets/images/nba-logos/wizards.png', 
       name: 'Wizards'
     }
 ]
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,15 +2,17 @@ import styles from "./home.module.scss";
 import Helmet from "react-helmet";
 import api from "../../services/api";
 import { useEffect, useState } from "react";
-// import { teams } from "../../assets/teams-logo";
+import { teams } from "../../assets/teams-logo";
+
 export const Home = () => {
   const [games, setGames] = useState([]);
+  
   useEffect(() => {
     const loadGames = async (endpoint) => {
       const options = {
         method: "GET",
         url: `https://v2.nba.api-sports.io${endpoint}/`,
-        params: { live: "all" },
+        params: { date: '2024-02-10' },
         headers: {
           "X-RapidAPI-Key": "bfbb0d0938f82376449382812ca2e42a",
           "X-RapidAPI-Host": "v2.nba.api-sports.io",
@@ -29,6 +31,16 @@ export const Home = () => {
     loadGames("/games");
   }, []);
 
+  const loadTeamLogo = (teamName) => {
+    for (const team of teams) {
+      if (teamName === team.name) {
+        console.log(team.picture_path)
+        return team.picture_path;
+      }
+    }
+    return null; // Se não encontrar o logotipo, retorne null ou algum valor padrão
+  };
+
   return (
     <>
       <Helmet>
@@ -37,23 +49,24 @@ export const Home = () => {
       <div className={styles.homeBody}>
         <div className={styles.dashboard}>
           <h1>Games In Live</h1>
-          {games === 0 ? (
-          <div className={styles.games}>
-            {games.map((game) => (
-              <div className={styles.gameInfo}>
-                
-                <div className={styles.title}>
-                  <span>{game.teams.home.nickname}</span>
-                  <strong>X</strong>
-                  <span>{game.teams.visitors.nickname}</span>
+          
+          {games ? (
+            <div className={styles.games}>
+              {games.map((game, index) => (
+                <div key={index} className={styles.gameInfo}>
+                  <div className={styles.title}>
+                    <img src={loadTeamLogo(game.teams.home.nickname)} alt="Home Team Logo"width="100px" height="100px"/>
+                    <span>{game.teams.home.nickname}</span>
+                    <strong>X</strong>
+                    <img src={loadTeamLogo(game.teams.visitors.nickname)} alt="Home Team Logo" width="100px"/>
+                    <span>{game.teams.visitors.nickname}</span>
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div>
-          ): (
+              ))}
+            </div>
+          ) : (
             <h1>Não há jogos</h1>
-          )
-}
+          )}
         </div>
       </div>
     </>
