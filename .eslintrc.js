d5ea02bf58310197f447dbf493369c83efbd383f
R3N4NR <renanramalhou@gmail.com>
2024-02-23 09:58:39 -0300

    implementa as configurações de lint no projeto

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: ['plugin:prettier/recommended'],
+  plugins: ['import', 'react'],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+    sourceType: 'module',
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+      experimentalObjectRestSpread: true,
+    },
+  },
+  rules: {
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+  },
+};
